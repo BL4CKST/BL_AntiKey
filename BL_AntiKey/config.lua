3b55fcfb0681c455415bc41dd6b44b6bc078a582
moisessalas94 <moises_sa@outlook.com>
2022-02-11 12:30:06 -0500

    Update config.lua

--- BL_AntiKey/config.lua
+++ BL_AntiKey/config.lua
@@ -1,18 +1,18 @@
 Config = {} 
 Config.language         = 'es' -- Available language (en / es)
-Config.KickEnable		    = true -- if true, will kick players who press the key.
+Config.KickEnable		= true -- if true, will kick players who press the key.
 Config.WebhookEnable    = true -- if true, send player information to the discord. / add the webhook in webhook.lua
 -------------------------------------------------------------------------------------------------------------------
-Config.AntiKeyInsert  	= true -- KEY INSERT
+Config.AntiKeyInsert 	= true -- KEY INSERT
 Config.AntiKeyControlF8 = true -- KEY CTRL + F8
 Config.AntiKeyALTF8     = true -- KEY ALT+F8
 Config.AntiKeyShiftG    = true -- KEY SHIFT+G
 Config.AntiKeyDelete    = true -- KEY DELETE
-Config.AntiKeyHome		  = true -- KEY HOME
-Config.AntiKeyPageUp	  = true -- KEY PAGEUP
+Config.AntiKeyHome		= true -- KEY HOME
+Config.AntiKeyPageUp	= true -- KEY PAGEUP
 Config.AntiKeyPageDown	= true -- KEY PAGEDOWN
-Config.AntiKeyTab		    = true -- KEY TAB
-Config.AntiKeyCaps	  	= true -- KEY CAPS
+Config.AntiKeyTab		= true -- KEY TAB
+Config.AntiKeyCaps		= true -- KEY CAPS
 -------------------------------------------------------------------------------------------------------------------
 --en
 Config.LangEN = { 
